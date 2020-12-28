236e59b50fb9a7fa4a84b801cbbc8c7aae580d6f
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-12-28 14:14:21 +0200

    Updates

--- Morris/classes/Game.js
+++ Morris/classes/Game.js
@@ -2,6 +2,10 @@ class Game {
     constructor() {
         this.dots = this.initDots()
         this.prevDot
+        this.playerB = new Player(color(0))
+        this.playerW = new Player(color(255))
+        this.turn = random(1) < 0.5 ? this.playerB : this.playerW
+        this.gameStarted = false
     }
     drawBoard() {
         push()
@@ -17,17 +21,24 @@ class Game {
         for (var layers = 0; layers < 3; layers++) {
             this.drawRect(outBoxSize - distance * layers)
         }
+        textSize(30)
+        fill(0)
+        textAlign(CENTER)
+        if (this.gameStarted) {
+            text("Turn:",-circleSize*3,+circleSize/5)
+        } else {
+            text("Place your chips", 0, +circleSize*3)
+        }
+        fill(this.turn.color)
+        noStroke()
+        circle(0,0,circleSize*3)
         pop()
     }
     drawRect(w) {
         push()
         rectMode(CENTER)
         noFill()
-        // strokeWeight(circleSize / 5)
-        // translate(x, y)
         rect(0, 0, w, w)
-
-
         for (var l in this.dots) {
             for (var d in this.dots[l]) {
                 var dot = this.dots[l][d]
@@ -54,7 +65,7 @@ class Game {
             rect.push(new Dot(-1, 0))
             dots.push(rect)
         }
-        console.log(dots)
+        //Adding neighbours to dots
         for (var l in dots) {
             for (var d in dots[l]) {
                 d = Number(d)
@@ -65,32 +76,46 @@ class Game {
             }
         }
 
-        for(var i = 1; i<8;i+=2) {
+        for (var i = 1; i < 8; i += 2) {
             dots[0][i].neighbours.push(dots[1][i])
-            dots[1][i].neighbours.push(dots[0][i],dots[2][i])
+            dots[1][i].neighbours.push(dots[0][i], dots[2][i])
             dots[2][i].neighbours.push(dots[1][i])
         }
-
-        
         return dots
     }
     click() {
-        if (this.prevDot) {
-            this.prevDot.highlight = false
-            for (var n of this.prevDot.neighbours) {
-                n.highlight = false
+        if (this.gameStarted) {
+            if (this.prevDot) {
+                this.prevDot.highlight = false
+                for (var n of this.prevDot.neighbours) {
+                    n.highlight = false
+                }
             }
-        }
-        for (var l in this.dots) {
-            for (var dot of this.dots[l]) {
-                var size = (outBoxSize - distance * l) / 2
-                if ((dot.player || !dot.player && this.prevDot) && pointInCircle(mX, mY, dot.x * size, dot.y * size, dot.r / 2)) {
-                    this.prevDot = dot.click(this.prevDot)? undefined : dot 
-                    return
+            for (var l in this.dots) {
+                for (var dot of this.dots[l]) {
+                    var size = (outBoxSize - distance * l) / 2
+                    if ((dot.player === this.turn || this.prevDot && this.prevDot.player === this.turn && !dot.player) && pointInCircle(mX, mY, dot.x * size, dot.y * size, dot.r / 2)) {
+                        this.prevDot = dot.click(this.prevDot) ? undefined : dot
+                        return
+                    }
+                }
+            }
+            this.prevDot = undefined
+        } else {
+            for (var l in this.dots) {
+                for (var dot of this.dots[l]) {
+                    var size = (outBoxSize - distance * l) / 2
+                    if (!dot.player && pointInCircle(mX, mY, dot.x * size, dot.y * size, dot.r / 2)) {
+                        this.turn.chipCount++
+                        dot.player = this.turn
+                        dot.r = circleSize * 2
+                        this.turn = this.turn === this.playerW ? this.playerB : this.playerW
+                        this.gameStarted = this.playerB.chipCount + this.playerW.chipCount === 4
+                    }
                 }
             }
         }
-        this.prevDot = undefined
+        
     }
     hover() {
         for (var l in this.dots) {
--- Morris/sketch.js
+++ Morris/sketch.js
@@ -4,21 +4,19 @@ var distance
 var cnv
 var scaledWidth
 var game
-var mX,mY
+var mX, mY
 const defaultWidth = 800
 function setup() {
 	cnv = createCanvas(defaultWidth, defaultWidth);
 	start()
 	windowResized()
-	
-	
 }
 function start() {
 	game = new Game()
 }
 function windowResized() {
-	scaledWidth = min(windowHeight, windowWidth,800)
-	cnv.resize(scaledWidth,scaledWidth)
+	scaledWidth = min(windowHeight, windowWidth, 800)
+	cnv.resize(scaledWidth, scaledWidth)
 	outBoxSize = width - 100
 	circleSize = width / 35
 	distance = width / 4
@@ -31,11 +29,11 @@ function draw() {
 	translate(width / 2, height / 2)
 	game.drawBoard()
 	game.hover()
-	textSize(30)
-	fill(0)
-	textAlign(CENTER)
-	mX = mouseX - width/2 
-	mY = mouseY - height/2
-	text(mX+","+mY,-100,height/2-10)
+	// textSize(30)
+	// fill(0)
+	// textAlign(CENTER)
+	mX = mouseX - width / 2
+	mY = mouseY - height / 2
+	// text(mX + "," + mY, -100, height / 2 - 10)
 }
 
