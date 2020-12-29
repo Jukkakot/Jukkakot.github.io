5fbd1ef171c85a4d11d7a8e3bece87c4c5cac0f1
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-12-29 22:35:45 +0200

    Updates

--- Connectfour/sketch.js
+++ Connectfour/sketch.js
@@ -46,11 +46,12 @@ function setup() {
   restartButton.style('font-size', chipRadius * 0.5 + "px")
   restartButton.style('background-color', color(200, 100))
   restartButton.mousePressed(() => start())
-  frameRate(30)
+  
   start()
 
 }
 function start() {
+  frameRate(30)
   chipRadius = 55
   engine = Engine.create()
   world = engine.world
@@ -80,7 +81,7 @@ function keyPressed(e) {
 function draw() {
   if (frameCount === 1) windowResized()
   background(100);
-  Engine.update(engine,40)
+  Engine.update(engine,50)
   game.show()
   //Displaying fps
   if (frameCount % 60 == 0) fps = frameRate()
